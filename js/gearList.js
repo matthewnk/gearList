f26103c57e7ef0ff9b98210aaee9b220183070e6
Matthew King <mrmatthew.nk@gmail.com>
2015-08-03 14:59:52 -0700

    Homepage Phase 1 complete

---
+++ js/gearList.js
@@ -0,0 +1,49 @@
+
+$(document).ready( function(){
+		displayGearList();
+		$("#addButton").click(addItem);
+		// $("#addButton").click(clearEntry);
+
+		$("#clearButton").click(function(){
+		  localStorage.removeItem('theList');
+		  $('#gearList').html(" ");
+		});
+	});
+
+function addItem(e) {
+  var entry = {};
+  entry.item = $("#item").val();
+
+  var dictionary = getDictionary();
+  dictionary.push(entry);
+  saveDictionary(dictionary);
+  displayGearList(getDictionary());
+  e.preventDefault();
+}
+
+function clearEntry (input, val) {
+	if (input.value == val)
+		input.value="";
+}
+
+function displayGearList(){
+  var d = getDictionary();
+  $gearList = $('#gearList');
+  $gearList.html(" ");
+  $.each(d, function(index, entry){
+    $gearList.append("<label>" + "<li>" + "<input type='checkbox'/>" + entry.item + "</label></li><dd>" + "</dd>");
+    	$("li").addClass("gearList")
+  });
+}
+
+function getDictionary(){
+  if (localStorage.getItem('theList') === null){
+    return([]);
+  } else {
+    return(JSON.parse(localStorage.getItem('theList')));
+  }
+}
+
+function saveDictionary(d) {
+  localStorage.setItem('theList', JSON.stringify(d));
+}
