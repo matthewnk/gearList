4a946f2a2da9f1eb8c6cd8b29cd2cbb158e6518e
Matthew King <mrmatthew.nk@gmail.com>
2015-08-10 21:41:18 -0700

    new fancy-style checkboxes

--- js/gearList1.js
+++ js/gearList1.js
@@ -26,25 +26,25 @@ $(function() {
   });
 
   // CLICK HANDLING - USES DELEGATION ON <ul> ELEMENT
-  $list.on('click', 'li', function() {
-    var $this = $(this);               // Cache the element in a jQuery object
-    var complete = $this.hasClass('complete');  // Is item complete
+  // $list.on('click', 'li', function() {
+  //   var $this = $(this);               
+  //   var complete = $this.hasClass('complete'); 
 
-    if (complete === true) {           // Check if item is complete
-      $this.animate({                  // If so, animate opacity + padding
-        opacity: 0.0,
-        paddingLeft: '+=180'
-      }, 500, 'swing', function() {    // Use callback when animation completes
-        $this.remove();                // Then completely remove this item
-      });
-    } else {                           // Otherwise indicate it is complete
-      item = $this.text();             // Get the text from the list item
-      $this.remove();                  // Remove the list item
-      $list                            // Add back to end of list as complete
-        .append('<li class=\"complete\">' + item + '</li>')
-        .hide().fadeIn(300);           // Hide it so it can be faded in
-      updateCount();                   // Update the counter
-    }                                  // End of else option
-  });                                  // End of event handler
+  //   if (complete === true) {           
+  //     $this.animate({                  
+  //       opacity: 0.0,
+  //       paddingLeft: '+=180'
+  //     }, 500, 'swing', function() {    
+  //       $this.remove();                
+  //     });
+  //   } else {                           
+  //     item = $this.text();             
+  //     $this.remove();                  
+  //     $list                            
+  //       .append('<li class=\"complete\">' + item + '</li>')
+  //       .hide().fadeIn(300);           
+  //     updateCount();                   
+  //   }                                  
+  // });                                  
 
 });
