32f6c078c1d1eb3f1d31d8d2fb9e59c461b6671b
Matthew King <mrmatthew.nk@gmail.com>
2015-08-10 22:30:58 -0700

    new checkbox styles

--- js/gearList1.js
+++ js/gearList1.js
@@ -7,7 +7,12 @@ $(function() {
   $newItemForm = $('#newItemForm');              // Cache form to add new items
   $newItemButton = $('#newItemButton');          // Cache button to show form
 
-
+  // ITEM COUNTER
+  function updateCount() {                       // Create function to update counter
+    var items = $('li').length; // Number of items in list
+    $('#counter').text(items);                   // Added into counter circle
+  }
+  updateCount();                                 // Call the function
 
   // SETUP FORM FOR NEW ITEMS
   $newItemButton.show();                         // Show the button
@@ -21,30 +26,30 @@ $(function() {
   $newItemForm.on('submit', function(e) {       // When a new item is submitted
     e.preventDefault();                         // Prevent form being submitted
     var text = $('input:text').val();           // Get value of text input
-    $list.append('<li>' + "<input type='checkbox'/>" + text + '</li>');      // Add item to end of the list
+    $list.append('<li>' + "<input type='checkbox' id='checkbox-1-'/>" + "<label for='checkbox-1-'>" + text + "</label>" + '</li>');      // Add item to end of the list
     $('input:text').val('');                    // Empty the text input
   });
 
   // CLICK HANDLING - USES DELEGATION ON <ul> ELEMENT
-  // $list.on('click', 'li', function() {
-  //   var $this = $(this);               
-  //   var complete = $this.hasClass('complete'); 
+//   $list.on('click', 'li', function() {
+//     var $this = $(this);               
+//     var complete = $this.hasClass('complete'); 
 
-  //   if (complete === true) {           
-  //     $this.animate({                  
-  //       opacity: 0.0,
-  //       paddingLeft: '+=180'
-  //     }, 500, 'swing', function() {    
-  //       $this.remove();                
-  //     });
-  //   } else {                           
-  //     item = $this.text();             
-  //     $this.remove();                  
-  //     $list                            
-  //       .append('<li class=\"complete\">' + item + '</li>')
-  //       .hide().fadeIn(300);           
-  //     updateCount();                   
-  //   }                                  
-  // });                                  
+//     if (complete === true) {           
+//       $this.animate({                  
+//         opacity: 0.0,
+//         paddingLeft: '+=180'
+//       }, 500, 'swing', function() {    
+//         $this.remove();                
+//       });
+//     } else {                           
+//       item = $this.text();             
+//       $this.remove();                  
+//       $list                            
+//         .append('<li class=\"complete\">' + item + '</li>')
+//         .hide().fadeIn(300);           
+//       updateCount();                   
+//     }                                  
+//   });                                  
 
 });
