c6a740a5337b6153bf2952b32ef306aaa6f4e755
Matthew King <mrmatthew.nk@gmail.com>
2015-09-14 13:34:14 -0700

    When the user adds an acitvity on the homepage, it is add to the list and converted into a link to a new page with theusers input as the new url.

---
+++ js/click.js
@@ -0,0 +1,12 @@
+var audiotypes = {
+	"mp3" : "audio/mpeg",
+	"ogg" : "audio/ogg",
+}
+
+function creatClick(sound){
+	var click1 = document.createElement('audio')
+	if (click1.canPlayType)
+		for (var i=0; i<arguments.length; i++) {
+			
+		}
+}
--- js/homeList.js
+++ js/homeList.js
@@ -17,34 +17,25 @@ $(function() {
     $newItemForm.show();                         // Show the form
   });
 
-  // ADDING A NEW LIST ITEM
+  // ADDING A NEW LIST
   $newItemForm.on('submit', function(e) {       // When a new item is submitted
     e.preventDefault();                         // Prevent form being submitted
     var text = $('input:text').val();           // Get value of text input
-    $list.append('<li>' + '<a href="">' + text + '</a>' + '</li>');      // Add item to end of the list
+    var pageUrl = window.location.hostname   //get current page url
+    var url = pageUrl + text + '.html';       //url
+    // url += $('#userList').val();
+    $list.append('<li class=user-' + text + 'List>' + text.link(url) + '</li>'); // Add item to end of the list
+    // window.location = url;
     $('input:text').val('');                    // Empty the text input
   });
 
+
   // CLICK HANDLING - USES DELEGATION ON <ul> ELEMENT
   $list.on('click', 'li', function() {
-    var $this = $(this);               // Cache the element in a jQuery object
-    var complete = $this.hasClass('complete');  // Is item complete
 
-    if (complete === true) {           // Check if item is complete
-      $this.animate({                  // If so, animate opacity + padding
-        opacity: 0.0,
-        paddingLeft: '+=180'
-      }, 500, 'swing', function() {    // Use callback when animation completes
-        $this.remove();                // Then completely remove this item
-      });
-    } else {                           // Otherwise indicate it is complete
-      item = $this.text();             // Get the text from the list item
-      $this.remove();                  // Remove the list item
-      $list                            // Add back to end of list as complete
-        .append('<li class=\"complete\">' + item + '</li>')
-        .hide().fadeIn(300);           // Hide it so it can be faded in
-      updateCount();                   // Update the counter
-    }                                  // End of else option
-  });                                  // End of event handler
+    // $("#userList").click(function() {
+        window.location = "file:///Users/MNK/Documents/PCS/gearList/" + $("#text").val();
+    // });
 
+  });                          
 });
